9cd990f641a389270aef91d7ccacb659f202da7c
Vasanth-B <vasanthdany25@gmail.com>
2025-03-18 01:25:30 +0530

    final

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
+// Create a root for ReactDOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside React.StrictMode
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// Optional: Measure performance in your app
+// Pass a function to log results (e.g., reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
